009daf23d560f044aead5b67ac930fa03591a56b
Игорь <>
2016-03-03 13:07:02 +0200

    ing_download

--- modules/loadedImgs.js
+++ modules/loadedImgs.js
@@ -1,5 +1,24 @@
+var fs = require('fs');
+var path = require('path');
+var config = require('./config');
+var fs2 = require('./fs2');
+
+var mimeTypes = {
+	'html': 'text/html',
+	'jpeg': 'image/jpeg',
+	'jpg': 'image/jpeg',
+	'png': 'image/png',
+	'js': 'text/javascript',
+	'css': 'text/css'
+};
+
 function loadedImgs(req, res){
 	console.log('images');
+	var filename = config.IMG_FOLDER + '/' +req.url.split('?')[0].split('/')[2];
+	var mimeType = mimeTypes[path.extname(filename).split('.')[1]];
+	res.writeHead(200, {'Content-Type': mimeType});
+	var fileStream = fs.createReadStream(filename);
+	fileStream.pipe(res);
 }
 
 module.exports = loadedImgs;
